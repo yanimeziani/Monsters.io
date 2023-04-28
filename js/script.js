89ac4b92cf8637c868c346bd6f1e417644e5d870
Francis Deslauriers <2391676@csfoy.ca>
2023-04-28 11:07:03 -0400

    on y arrive

--- js/script.js
+++ js/script.js
@@ -1,19 +1,92 @@
+"use strict"
 // Définir les variables
 let form = document.querySelector("#monsterForm");
+let monsterName = document.querySelector("#monsterName");
+let monsterDescription = document.querySelector("#monsterDescription");
+let monsterAttack = document.querySelector("#attackPoints");
+let monsterDefense = document.querySelector("#defensePoints");
+let monsterHeatlh = document.querySelector("#healthPoints");
+let monsterTotalPoints = document.querySelector("#totalPoints");
+
+
+let bossAttack = document.querySelector("#bossAttackPoints");
+let bossDefense = document.querySelector("#bossDefensePoints");
+let bossHeatlh = document.querySelector("#bossHP");
+let errorMessage= document.querySelector("#error");
+let nbrOfMonsters = 0;
+//console.log(monsterType[0].checked + monsterBackground[0].checked)
+
+
 
 // Définir les événements
 form.addEventListener("submit", validation);
-
+monsterAttack.addEventListener("input", updateFields);
+monsterDefense.addEventListener("input", updateFields);
+monsterHeatlh.addEventListener("input", updateFields);
 // Définir les fonctions
 function validation(event) {
   // Vérifier si le formulaire est valide
   // Annuler l'envoi du formulaire
+  let errorMessages = [];
   event.preventDefault();
+  if(monsterName.value.length > 16){
+    errorMessages.push("Le nom du monstre ne doit pas dépasser 16 caractères");
+  }
 
-  // Si le formulaire est valide, appeler la fonction create() et reset le formulaire
+  if(monsterDescription.value.length > 128){
+    errorMessages.push("La description du monstre ne doit pas dépasser 128 caractères");
+  }
+  if(errorMessages.length > 0){
+  addErrorMessage(errorMessages);
+}
+else{
+  errorMessage.classList.add("hidden");
+  create();
   form.reset();
 }
+}
+
 
 function create() {
   // Créer un élément dans la liste de cartes
+  let monsterBackground = document.querySelector("input[name='monsterBackground']:checked");
+  if(nbrOfMonsters == 5){
+    return}
+  nbrOfMonsters++;
+  let card=document.querySelector("#monsterCard"+nbrOfMonsters);
+  let monsterType = document.querySelector("input[name='monsterType']:checked");
+  card.classList.remove("greyBackground");
+  card.classList.add(monsterBackground.value+"Background");
+card.innerHTML+='<h3>'+monsterName.value+'</h3>';
+card.innerHTML+='<img src="img/realMonsters/'+monsterType.value+'.png"/>';
+card.innerHTML+='<p>'+monsterDescription.value+'</p>';
+}
+
+function updateFields(event) {
+//event.target.value = parseInt(event.target.value);
+  if (!isNaturalNumber(event.target.value)) {
+    event.target.value = 0;
+  }
+  let points = parseInt(monsterAttack.value) + parseInt(monsterDefense.value) + parseInt(monsterHeatlh.value);
+  if(points>10){
+    monsterTotalPoints.classList.add("error")
+}
+monsterTotalPoints.value = points+"/10";
+}
+
+//cette fonction a été prise sur stackoverflow
+function isNaturalNumber(n) {
+  n = n.toString(); // force the value incase it is not
+  let n1 = Math.abs(n),
+      n2 = parseInt(n, 10);
+  return !isNaN(n1) && n2 === n1 && n1.toString() === n;
+}
+
+function addErrorMessage(message){
+  errorMessage.innerHTML = "";
+  for(let i=0; i<message.length; i++){
+  errorMessage.innerHTML += "<p>"+message[i]+"</p>";
+  
+}
+errorMessage.classList.remove("hidden");
 }
